9f397abd4cc2ede1a683e4ccf21f5461afbe545b
Leko <leko.noor@gmail.com>
2014-10-02 02:20:10 +0900

    Savings.phpの自動生成で不適切な箇所を修正

--- source/Bolster/BaseApi/Savings.php
+++ source/Bolster/BaseApi/Savings.php
@@ -8,7 +8,6 @@ namespace Bolster\BaseApi;
  */
 class Savings extends Client
 {
-
 	/**
 	 * 引き出し申請情報の一覧を取得
 	 * 
@@ -16,10 +15,10 @@ class Savings extends Client
 	 * @see https://gist.github.com/baseinc/5c338fa3a62e870f9597
 	 * 
 	 * @param array $params 指定可能なオプションは以下を参照
-	 *   @param XXXXX start_created 引き出し申請日時はじめ yyyy-mm-dd (任意)
-	 *   @param XXXXX end_created 引き出し申請日時おわり yyyy-mm-dd (任意)
-	 *   @param XXXXX limit リミット (任意 デフォルト: 20, MAX: 100)
-	 *   @param XXXXX offset オフセット (任意 デフォルト: 0)
+	 *   @param string start_created 引き出し申請日時はじめ yyyy-mm-dd (任意)
+	 *   @param string end_created   引き出し申請日時おわり yyyy-mm-dd (任意)
+	 *   @param int    limit         リミット (任意 デフォルト: 20, MAX: 100)
+	 *   @param int    offset        オフセット (任意 デフォルト: 0)
 	 * @return array 連想配列。ドキュメントのサンプルレスポンスを参照
 	 */
 	public function savings(array $params = array()) {
