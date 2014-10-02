b85058c1614533f2954ccbaec0018d7dd88efa3c
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-10-02 22:00:38 +0900

    通常エラーコードに数値以外を格納できないのでコンストラクタを拡張

--- source/Bolster/BaseApi/BaseApiException.php
+++ source/Bolster/BaseApi/BaseApiException.php
@@ -6,4 +6,9 @@ namespace Bolster\BaseApi;
  * BASE APIへリクエストを行った時に発生した例外
  * @author Leko <leko.noor@gmail.com>
  */
-class BaseApiException extends \RuntimeException {}
+class BaseApiException extends \RuntimeException {
+	public function __construct($message, $code) {
+		$this->code = $code;
+		return parent::__construct($message);
+	}
+}
