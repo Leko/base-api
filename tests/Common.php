68b013c48cefb36d17ac096b1773a6f58b7dde7b
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-10-03 11:28:52 +0900

    OAuth#authorizeのテストを追加

--- tests/Common.php
+++ tests/Common.php
@@ -3,5 +3,17 @@
 namespace Bolster\BaseApi\Tests;
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../source/BaseApi/Client.php';
+require_once __DIR__.'/../source/BaseApi/OAuth.php';
+require_once __DIR__.'/../source/BaseApi/Users.php';
+require_once __DIR__.'/../source/BaseApi/Items.php';
+require_once __DIR__.'/../source/BaseApi/Categories.php';
+require_once __DIR__.'/../source/BaseApi/ItemCategories.php';
+require_once __DIR__.'/../source/BaseApi/Orders.php';
+require_once __DIR__.'/../source/BaseApi/Savings.php';
+require_once __DIR__.'/../source/BaseApi/HttpRequestable.php';
+require_once __DIR__.'/../source/BaseApi/BaseApiException.php';
+require_once __DIR__.'/../source/BaseApi/ExpiredAccessTokenException.php';
+require_once __DIR__.'/../source/BaseApi/RateLimitExceedException.php';
 
 abstract class Common extends \PHPUnit_Framework_TestCase {}
--- tests/OAuth.php
+++ tests/OAuth.php
@@ -4,4 +4,52 @@ namespace Bolster\BaseApi\Tests;
 
 require_once __DIR__.'/Common.php';
 
-class OAuth extends Common {}
+class MockHttpClient extends \Bolster\Http implements \Bolster\BaseApi\HttpRequestable {}
+
+class OAuth extends Common {
+	private $client;
+
+	public function setUp()
+	{
+		$this->client = new \Bolster\BaseApi\Client([
+			'client_id' => CLIENT_ID,
+			'client_secret' => CLIENT_SECRET,
+			'redirect_uri' => REDIRECT_URI,
+		]);
+		$this->client->setHttpClient(new MockHttpClient());
+	}
+
+	function test_authorize_戻り値のURLのプロトコルはhttps() {
+		$url = $this->client->oauth()->authorize();
+		$parsed = parse_url($url);
+
+		$this->assertEquals('https', $parsed['scheme']);
+	}
+	function test_authorize_戻り値のURLのホストはapi_thebase_in() {
+		$url = $this->client->oauth()->authorize();
+		$parsed = parse_url($url);
+
+		$this->assertEquals('api.thebase.in', $parsed['host']);
+	}
+	function test_authorize_戻り値のURLにresponse_typeが含まれている() {
+		$url = $this->client->oauth()->authorize();
+		$parsed = parse_url($url);
+		parse_str($parsed['query'], $query);
+
+		$this->assertArrayHasKey('response_type', $query);
+	}
+	function test_authorize_戻り値のURLにclient_idが含まれている() {
+		$url = $this->client->oauth()->authorize();
+		$parsed = parse_url($url);
+		parse_str($parsed['query'], $query);
+
+		$this->assertArrayHasKey('client_id', $query);
+	}
+	function test_authorize_戻り値のURLにredirect_uriが含まれている() {
+		$url = $this->client->oauth()->authorize();
+		$parsed = parse_url($url);
+		parse_str($parsed['query'], $query);
+
+		$this->assertArrayHasKey('redirect_uri', $query);
+	}
+}
